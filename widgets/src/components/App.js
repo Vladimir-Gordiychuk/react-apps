f7d1da807360f4a66a2ea2ca9c2febe2a5bb8c9d
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-10 14:08:07 +0200

    Updated Accordion to render items passes as props.

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -2,10 +2,25 @@ import './App.css';
 
 import Accordion from './Accordion';
 
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front-end JavaScript framework.'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers.'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use react by creating components.'
+    }
+];
+
 export default () => {
     return (
         <div>
-            <Accordion />
+            <Accordion items={items}/>
         </div>
     );
 }
