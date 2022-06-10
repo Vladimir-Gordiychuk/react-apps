bf2d5554bdbe755a81060614bd0231f7a3c612fd
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-10 19:44:27 +0200

    widgets : added new Search component.

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 
 import Accordion from './Accordion';
+import Search from './Search';
 
 const items = [
     {
+        id: 1,
         title: 'What is React?',
         content: 'React is a front-end JavaScript framework.'
     },
     {
+        id: 2,
         title: 'Why use React?',
         content: 'React is a favorite JS library among engineers.'
     },
     {
+        id: 3,
         title: 'How do you use React?',
         content: 'You use react by creating components.'
     }
@@ -20,7 +24,7 @@ const items = [
 export default () => {
     return (
         <div>
-            <Accordion items={items}/>
+            <Search />
         </div>
     );
 }
---
+++ widgets/src/components/Search.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react';
+
+import wikipedia from '../apis/wikipedia';
+import Accordion from './Accordion';
+
+const Search = () => {
+
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    useEffect(() => console.log('every render'));
+    useEffect(() => console.log('first render only'), []);
+
+    useEffect(() => {
+        console.log('"term" changed -> ' + term)
+
+        if (term.length === 0)
+            return;
+
+        wikipedia.get('', {
+            params: {
+                srsearch: term
+            }
+        }).then((response) => {
+            console.log(response.data.query.search);
+            if (response.data && !response.data.error) {
+                setResults(response.data.query.search.map(item => {
+                    return {
+                        id: item.pageId,
+                        title: item.title,
+                        content: item.snippet
+                    };
+                }));
+            }
+        });
+    }, [term]);
+
+    const onChange = (event) => {
+        setTerm(event.target.value);
+    }
+
+    return (
+        <div>
+            <div className="ui segment">
+                <div className="ui form">
+                    <div className="field">
+                        <label htmlFor="query">Search for</label>
+                        <input id="query" value={term} onChange={onChange} />
+                    </div>
+                </div>
+            </div>
+            <Accordion items={results}/>
+        </div>
+    );
+
+};
+
+export default Search;
