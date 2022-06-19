d549cabc8ebb29a532b587389ea1796d9fcc0161
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-19 12:56:43 +0200

    context: Extracted LanguageSelector component from the App component.

--- context/src/components/App.js
+++ context/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
     state = { language: 'english' };
@@ -16,13 +17,7 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <div className="ui segment">
-                    <div className="ui dividing header">
-                        Select a language:
-                        <i className="flag us"
-                            onClick={() => this.setLanguage('english')} />
-                        <i className="flag nl"
-                            onClick={() => this.setLanguage('dutch')} />
-                    </div>
+                    <LanguageSelector onLanguageChange={this.setLanguage} />
                     <LanguageContext.Provider value={this.state.language}>
                         <ColorContext.Provider value={{
                             backgroundColor: 'grey',
